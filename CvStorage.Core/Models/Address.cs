18a21c343469c191a8dec78ec3d1a0e8414739d0
Andrejs <andrejs.boja@gmail.com>
2021-07-19 12:16:48 +0300

    addMigration

--- CvStorage.Core/Models/Address.cs
+++ CvStorage.Core/Models/Address.cs
@@ -6,6 +6,6 @@
         public string City { get; set; }
         public string Street { get; set; }
         public string PostCode { get; set; }
-        public int StreetNumber { get; set; }
+        public string StreetNumber { get; set; }
     }
 }
---
+++ CvStorage.Data/Migrations/202107190911438_ChangedStreetnumberTOstring.cs
@@ -0,0 +1,18 @@
+﻿namespace CvStorage.Data.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class ChangedStreetnumberTOstring : DbMigration
+    {
+        public override void Up()
+        {
+            AlterColumn("dbo.Addresses", "StreetNumber", c => c.String());
+        }
+        
+        public override void Down()
+        {
+            AlterColumn("dbo.Addresses", "StreetNumber", c => c.Int(nullable: false));
+        }
+    }
+}
